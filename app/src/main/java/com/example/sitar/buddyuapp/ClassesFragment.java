ed817a85615a7f5f1e51b07a011da1c308fe7bf2
sitarobinson <sita.robinson@gmail.com>
2017-05-07 10:13:36 -0400

    Adding Classes to Classes Screen

--- app/src/main/java/com/example/sitar/buddyuapp/ClassesFragment.java
+++ app/src/main/java/com/example/sitar/buddyuapp/ClassesFragment.java
@@ -1,13 +1,34 @@
 package com.example.sitar.buddyuapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,12 +41,18 @@ import android.view.ViewGroup;
 public class ClassesFragment extends android.app.Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String TAG="ClassesFragment";
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private FirebaseAuth firebaseAuth;
+    private ListView listView;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -50,6 +77,63 @@ public class ClassesFragment extends android.app.Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState)
+    {
+        firebaseAuth=firebaseAuth.getInstance();
+        DatabaseReference catalog = FirebaseDatabase.getInstance().getReference("Catalog");
+        listView = (ListView) view.findViewById(R.id.courses);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        final ArrayList courses = new ArrayList();
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(),
+                android.R.layout.simple_list_item_multiple_choice, courses);
+
+
+        listView.setAdapter(adapter);
+
+
+        catalog.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot c_item: dataSnapshot.getChildren())
+                {
+                    String c_name=c_item.child("college").getValue(String.class);
+
+                    for (DataSnapshot s_item: c_item.child("subjects").getChildren())
+                    {
+                        String s_name = s_item.child("subject").getValue(String.class);
+                        courses.add("> " + c_name + " / " + s_name);
+
+                        for (DataSnapshot co_item: s_item.child("courses").getChildren())
+                        {
+                            String co_name = co_item.child("subject_code").getValue(String.class) + " " +
+                                    co_item.child("course_no").getValue(String.class) + " - " +
+                                    co_item.child("course_title").getValue(String.class);
+
+                            courses.add(co_name);
+                        }
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+        final Activity thisActivity = this.getActivity();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG,"Item clicked "+ position);
+            }
+        });
+
+    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
